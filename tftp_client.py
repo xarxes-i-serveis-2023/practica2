f2ddeeeaec7570dd916b92ebbbd986264ec0b276
piroliro <srpiroliro21@gmail.com>
2023-10-31 18:28:45 +0100

    fini

--- tftp_client.py
+++ tftp_client.py
@@ -5,102 +5,78 @@ import sys
 from struct import pack, unpack
 from os.path import exists, basename
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s", stream=sys.stdout)
+logging.basicConfig(level=logging.INFO,format="%(asctime)s %(levelname)s %(message)s", stream=sys.stdout,)
 
 class TFTPController:
     TFPT_OPCODES = {"RRQ": 1, "WRQ": 2, "DATA": 3, "ACK": 4, "ERROR": 5}
-    TFPT_MODES = {"octet"}
 
     HEADER_SIZE = 4
     DATA_SIZE = 512
     MAX_RETRIES = 3
     TIMEOUT = 1
-    
+
     class Packet:
-        code:int
-        message:str
-        block_number:int
-        data:bytes
+        code: int
+        message: str
+        block_number: int
+        data: bytes
 
     class Error(Packet):
         def __init__(self, data: bytes, encoding: str = "utf-8") -> None:
-            self.code:int = unpack("!H", data[2:4])[0]
-            self.message:str = data[4:].decode(encoding)
+            self.code: int = unpack("!H", data[2:4])[0]
+            self.message: str = data[4:].decode(encoding)
 
     class Ack(Packet):
         def __init__(self, data: bytes) -> None:
-            self.block_number:int = unpack("!H", data[2:4])[0]
+            self.block_number: int = unpack("!H", data[2:4])[0]
 
     class Data(Packet):
         def __init__(self, data: bytes) -> None:
-            self.block_number:int = unpack("!H", data[2:4])[0]
-            self.data:bytes = data[4:]
+            self.block_number: int = unpack("!H", data[2:4])[0]
+            self.data: bytes = data[4:]
 
-    ##########################################################################################
-
-    def __init__(self, server_ip: str, port: int, socket_timeout:float=TIMEOUT) -> None:
+    def __init__(self, server_ip: str, port: int, socket_timeout: float = TIMEOUT) -> None:
         self.socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket_udp.settimeout(socket_timeout) 
+        self.socket_udp.settimeout(socket_timeout)
         self.server_address = (server_ip, port)
-        
+
         self.logger = logging.getLogger(__name__)
 
-    # RRQ / WRQ
-    def send_request(
-        self, opcode: int, filename: str, mode: str = "octet", encoding: str = "utf-8"
-    ) -> None:
-        _format = f"!H{len(filename)}sB{len(mode)}sB"
-        self.logger.debug(f"Sending {opcode} request: {filename}; format: {_format}")
-        
+    def send_request(self, opcode: int, filename: str, mode: str = "octet", encoding: str = "utf-8") -> None:
+        self.logger.info(f"Sending {opcode} request: {filename}")
+
         packet_data = pack(
-            _format,
-            opcode,
-            bytes(filename, encoding),
-            0,
-            bytes(mode, encoding),
-            0,
-        )
+            f"!H{len(filename)}sB{len(mode)}sB", opcode, bytes(filename, encoding), 0, bytes(mode, encoding), 0)
 
         self.socket_udp.sendto(packet_data, self.server_address)
 
-    # DATA
     def send_data(self, block: int, data: bytes) -> None:
-        self.logger.debug(f"Sending DATA block_number={block}, data_length={len(data)}")
-
-        _format = f"!HH" # {len(data)}s
-        packet_data = pack(_format, self.TFPT_OPCODES["DATA"], block) + data
-        
-        print(packet_data)
+        self.logger.info(f"Sending DATA block_number={block}, data_length={len(data)}")
+        packet_data = pack(f"!HH{len(data)}s", self.TFPT_OPCODES["DATA"], block, data)
 
         self.socket_udp.sendto(packet_data, self.server_address)
 
-    # ACK
     def send_ack(self, block: int) -> None:
-        self.logger.debug(f"Sending ACK[{block}]")
-
-        self.socket_udp.sendto(
-            pack("!HH", self.TFPT_OPCODES["ACK"], block), self.server_address
-        )
-
-    ##########################################################################################
+        self.logger.info(f"Sending ACK[{block}]")
+        self.socket_udp.sendto(pack("!HH", self.TFPT_OPCODES["ACK"], block), self.server_address)
 
     def receive_data(self) -> tuple:
-        self.logger.debug("Waiting for packet...")
+        self.logger.info("Waiting for packet...")
 
         for _ in range(self.MAX_RETRIES):
             try:
                 return self.socket_udp.recvfrom(self.HEADER_SIZE + self.DATA_SIZE)
             except socket.timeout:
-                self.logger.debug("Timeout exceeded!")
+                self.logger.info("Timeout exceeded!")
             except Exception as e:
                 raise e
 
-        self.logger.debug("Max retries exceeded!")
+        self.logger.info("Max retries exceeded!")
         raise Exception("Max retries exceeded!")
 
     def transform_data(self, data: bytes):
-        self.logger.debug(f"Recieved data size: {len(data)}")
-        
+        self.logger.info(f"Recieved data size: {len(data)}")
+
         opcode = unpack("!H", data[:2])[0]
 
         if opcode == self.TFPT_OPCODES["ERROR"]:
@@ -117,7 +93,7 @@ class TFTPController:
         return self.transform_data(data)
 
     def split_file(self, local_filename: str):
-        self.logger.debug(f"Splitting file: {local_filename}")
+        self.logger.info(f"Splitting file: {local_filename}")
 
         splitted_file = []
         try:
@@ -139,7 +115,9 @@ class TFTPController:
             raise e
 
     def expect_packet(self, block_number: int, packet_type):
-        self.logger.debug(f"Expecting {packet_type.__name__.upper()} with block_number = {block_number}")
+        self.logger.info(
+            f"Expecting {packet_type.__name__.upper()} with block_number = {block_number}"
+        )
 
         answer_packet = self.get_packet()
         if type(answer_packet) is self.Error:
@@ -155,7 +133,6 @@ class TFTPController:
 
         return answer_packet
 
-
     def put(self, local_filename: str) -> None:
         if not exists(local_filename):
             raise Exception(f"File '{local_filename}' not found!")
@@ -163,14 +140,16 @@ class TFTPController:
         self.send_request(self.TFPT_OPCODES["WRQ"], local_filename)
         self.expect_packet(0, self.Ack)
 
-        for block_number, data in enumerate(self.split_file(local_filename)): # ,start=1): # SERVER ERROR: Data should start from 1. RFC1350
+        for block_number, data in enumerate(
+            self.split_file(local_filename)
+        ):  # ,start=1): # SERVER ERROR: Data should start from 1. RFC1350
             self.send_data(block_number, data)
 
             # SERVER ERROR: first ACK after first DATA, is empty.
             try:
                 self.expect_packet(block_number, self.Ack)
             except Exception as e:
-                if not block_number==0:
+                if not block_number == 0:
                     raise e
 
     def get(self, remote_filename: str) -> None:
@@ -182,23 +161,40 @@ class TFTPController:
         while True:
             data_packet = self.expect_packet(block_number, self.Data)
 
-            file_contents+=data_packet.data
+            file_contents += data_packet.data
             self.send_ack(data_packet.block_number)
 
             if len(data_packet.data) < self.DATA_SIZE:
                 break
 
             block_number = data_packet.block_number + 1
-        
+
         with open(basename(remote_filename), "wb") as f:
             f.write(file_contents)
-    
-    
+
     def close(self):
         self.socket_udp.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='TFTP client.')
     
-if __name__=="__main__":
-    t=TFTPController("127.0.0.1", 6969)
-    # t.get("as.txt")
-    t.put("data.txt")
-    t.close()
+    parser.add_argument('action', choices=['get', 'put'], help="The action to be performed. 'get' to download a file and 'put' to upload a file.")
+    parser.add_argument('filename', help="The name of the file to upload/download.")
+    parser.add_argument('server', help="The IP address or hostname of the TFTP server.")
+    parser.add_argument('-p', '--port', type=int, default=69, help="The port number of the TFTP server. Default is 69.")
+    parser.add_argument('-t', '--timeout', type=float, default=TFTPController.TIMEOUT, help="Socket timeout in seconds. Default is 1 second.")
+    
+    args = parser.parse_args()
+    
+    client = TFTPController(args.server, args.port, args.timeout)
+    
+    try:
+        if args.action == "put":
+            client.put(args.filename)
+        elif args.action == "get":
+            client.get(args.filename)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client.close()
