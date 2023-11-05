95c1a1ec51426f63b70519d9dfad67dbc791f2ec
piroliro <srpiroliro21@gmail.com>
2023-11-05 22:31:26 +0100

    solved: bug with files multiple of 512B

--- tftp_client.py
+++ tftp_client.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 from struct import pack, unpack
 from os.path import exists, basename
+from venv import create
 
 logging.basicConfig(level=logging.INFO,format="%(asctime)s %(levelname)s %(message)s", stream=sys.stdout)
 
@@ -14,6 +15,7 @@ class TFTPController:
     DATA_SIZE = 512
     MAX_RETRIES = 3
     TIMEOUT = 1
+    STRING_TERMINATOR = 0
 
     class Packet:
         code: int
@@ -35,6 +37,7 @@ class TFTPController:
             self.block_number: int = unpack("!H", data[2:4])[0]
             self.data: bytes = data[4:]
 
+
     def __init__(self, server_ip: str, port: int, socket_timeout: float = TIMEOUT) -> None:
         self.socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket_udp.settimeout(socket_timeout)
@@ -46,7 +49,7 @@ class TFTPController:
         self.logger.info(f"Sending {opcode} request: {filename}")
 
         packet_data = pack(
-            f"!H{len(filename)}sB{len(mode)}sB", opcode, bytes(filename, encoding), 0, bytes(mode, encoding), 0)
+            f"!H{len(filename)}sB{len(mode)}sB", opcode, bytes(filename, encoding), self.STRING_TERMINATOR, bytes(mode, encoding), self.STRING_TERMINATOR)
 
         self.socket_udp.sendto(packet_data, self.server_address)
 
@@ -122,7 +125,7 @@ class TFTPController:
 
         answer_packet = self.listen_packet()
         if type(answer_packet) is self.Error:
-            raise Exception(f"Error[{answer_packet.code}]: {answer_packet.message}")
+            raise Exception(f"ServerError[{answer_packet.code}]: {answer_packet.message}")
 
         elif type(answer_packet) is not packet_type:
             raise Exception(f"Invalid packet received! ({type(answer_packet)})")
@@ -133,6 +136,22 @@ class TFTPController:
             )
 
         return answer_packet
+    
+    def send_slice(self, block_number: int, data: bytes)->None:
+        # send a slice of data and wait for ACK. If the ACK is not the expected one, throw an exception.
+        
+        self.send_data(block_number, data)
+        
+        # SERVER ERROR: the first ACK after first DATA, it's empty.
+        if block_number == 0: return
+        
+        self.expect_packet(block_number, self.Ack)
+    
+    def get_slice(self, block_number: int)->Packet:
+        data_packet = self.expect_packet(block_number, self.Data)
+        self.send_ack(data_packet.block_number)
+        
+        return data_packet
 
     def put(self, local_filename: str) -> None:
         # send file to server
@@ -144,48 +163,51 @@ class TFTPController:
         self.expect_packet(0, self.Ack)
 
         for block_number, data in enumerate(self.split_file(local_filename)):  # ,start=1): # SERVER ERROR: Data should start from 1. RFC1350
-            self.send_data(block_number, data)
+            self.send_slice(block_number, data)
+        else:
+            if len(data)==self.DATA_SIZE: 
+                self.send_slice(block_number+1, b"")
+                # file could be exactly multiple of 512 bytes, so we need to send an empty packet to signal the end of the file.
 
-            # SERVER ERROR: first ACK after first DATA, is empty.
-            try:
-                self.expect_packet(block_number, self.Ack)
-            except Exception as e:
-                if not block_number == 0:
-                    raise e
         self.logger.info(f"File uploaded: {local_filename}")
 
     def get(self, remote_filename: str) -> None:
         # get file from server
 
-        self.send_request(self.TFPT_OPCODES["RRQ"], remote_filename)
-
         block_number = 1
+        
+        self.send_request(self.TFPT_OPCODES["RRQ"], remote_filename)
+        data_packet = self.get_slice(block_number)
+
         with open(basename(remote_filename), "wb") as f:
+            f.write(data_packet.data)
+            
             while True:
-                data_packet = self.expect_packet(block_number, self.Data)
-                self.send_ack(data_packet.block_number)
+                data_packet = self.get_slice(block_number)
 
                 f.write(data_packet.data)
                 if len(data_packet.data) < self.DATA_SIZE:
                     break
 
-                block_number = data_packet.block_number + 1
+                block_number += 1
+        
         self.logger.info(f"File downloaded: {remote_filename}")
 
     def close(self):
         self.socket_udp.close()
 
-
-if __name__ == "__main__":
+def create_parser()->argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='TFTP client.')
-    
     parser.add_argument('action', choices=['get', 'put'], help="The action to be performed. 'get' to download a file and 'put' to upload a file.")
     parser.add_argument('filename', help="The name of the file to upload/download.")
     parser.add_argument('server', help="The IP address or hostname of the TFTP server.")
     parser.add_argument('-p', '--port', type=int, default=69, help="The port number of the TFTP server. Default is 69.")
     parser.add_argument('-t', '--timeout', type=float, default=TFTPController.TIMEOUT, help="Socket timeout in seconds. Default is 1 second.")
     
-    args = parser.parse_args()
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args=create_parser()
     
     client = TFTPController(args.server, args.port, args.timeout)
     try:
@@ -194,6 +216,6 @@ if __name__ == "__main__":
         elif args.action == "get":
             client.get(args.filename)
     except Exception as e:
-        print(f"Error: {e}")
-    finally:
+        client.logger.error(e)
+    finally: 
         client.close()
